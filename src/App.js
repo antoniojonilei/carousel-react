da89c41a0810c04ea557c9e6d78ca2b23f21749c
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2022-12-14 15:59:38 -0300

    calculate width

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
+import { useState, useEffect, useRef } from 'react'
 import './App.css';
 import { motion } from 'framer-motion'
 
-import image1 from './img/img1.jpg'
-import image2 from './img/img2.jpg'
-import image3 from './img/img3.jpg'
-import image4 from './img/img4.jpg'
+import image1 from './img/img1-1000.jpg'
+import image2 from './img/img22.jpg'
+import image3 from './img/img33.jpg'
+import image4 from './img/img44.jpg'
 
 const images = [image1, image2, image3, image4]
 
 function App() {
+  const carousel = useRef()
+  const [width, setWidth] = useState(0)
+  
+  console.log(width)
+
+  useEffect(() => {
+    // console.log(carousel.current?.scrollWidth, carousel.current?.offsetWidth)
+    setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth)
+  }, [])
+
   return (
     <div className="App">
       
-      <motion.div className="carousel">
-        <motion.div className="inner">
+      <motion.div ref={carousel} className="carousel" whileTap={{ cursor: "grabbing" }}>
+        <motion.div 
+        className="inner" 
+        drag="x" 
+        dragConstraints={{ right:0, left: - 420  }} //usar o width (cálculo)
+        initial={{ x: 100 }}
+        animate={{ x: 0 }}
+        >
 
           {images.map(image => (
             <motion.div key={image} className="item">
